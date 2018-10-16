e587ba80dcfed66a2e563ec74d4a05a243fd4748
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-10-16 09:46:13 +0300

    add tasks and celery

--- suziScraper/urls.py
+++ suziScraper/urls.py
@@ -2,8 +2,11 @@ from django.conf import settings
 from django.conf.urls import url, static
 from django.views.generic import TemplateView
 from main.views import HomeView, get_data
+from django.contrib import admin
+from django.urls import path
 
 urlpatterns = [
     url(r'^$', HomeView.as_view(), name='home'),
     url(r'^api/get_data/', get_data, name='get_data'),
+    path('admin/', admin.site.urls),
 ]
