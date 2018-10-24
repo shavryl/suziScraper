bd03311a5c507bfbd74da00df952283b34937ec1
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-10-24 09:59:31 +0300

    model save method and userserializer

--- snippets/views.py
+++ snippets/views.py
@@ -1,7 +1,3 @@
-from django.http import Http404
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
 from snippets.models import Snippet
 from snippets.serializers import SnippetSerializer
 from rest_framework import generics
