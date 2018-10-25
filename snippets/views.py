95aa0ed9d30dfaacbea1d7714b3db4b3d0c53e19
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-10-25 10:05:56 +0300

    highlighted field and hyperlinkModelSerializers

--- snippets/views.py
+++ snippets/views.py
@@ -7,6 +7,7 @@ from rest_framework import permissions
 from rest_framework.decorators import api_view
 from rest_framework.response import Response
 from rest_framework.reverse import reverse
+from rest_framework import renderers
 
 
 class SnippetList(generics.ListCreateAPIView):
@@ -33,6 +34,15 @@ class UserList(generics.ListAPIView):
 class UserDetail(generics.RetrieveAPIView):
     queryset = User.objects.all()
     serializer_class = UserSerializer
+
+
+class SnippetHighlight(generics.GenericAPIView):
+    queryset = Snippet.objects.all()
+    renderer_classes = renderers.StaticHTMLRenderer
+
+    def get(self, request, *args, **kwargs):
+        snippet = self.get_object()
+        return Response(snippet.highlighted)
 
 
 @api_view(['GET'])
