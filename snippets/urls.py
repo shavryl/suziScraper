b85be62a4a37e5295542fc1f25299b456872a5d7
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-10-25 11:03:01 +0300

    suffix patterns

--- snippets/urls.py
+++ snippets/urls.py
@@ -1,6 +1,29 @@
 from django.urls import path
 from snippets import views
+from snippets.views import SnippetViewSet, UserViewSet, api_root
 from rest_framework.urlpatterns import format_suffix_patterns
+from rest_framework import renderers
+
+
+snippet_list = SnippetViewSet.as_view({
+    'get': 'list',
+    'post': 'create'
+})
+snippet_detail = SnippetViewSet.as_view({
+    'get': 'retrieve',
+    'put': 'update',
+    'patch': 'partial_update',
+    'delete': 'destroy'
+})
+snippet_highlight = SnippetViewSet.as_view({
+    'get': 'highlight'
+}, renderer_classes=[renderers.StaticHTMLRenderer])
+user_list = UserViewSet.as_view({
+    'get': 'list'
+})
+user_detail = UserViewSet.as_view({
+    'get': 'retrieve'
+})
 
 
 urlpatterns = [
