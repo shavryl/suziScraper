46f911b60cb999f3de9db4f97b7193750973a069
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-10-29 08:10:16 +0200

    Celery settings

--- suziScraper/celery.py
+++ suziScraper/celery.py
@@ -1,6 +1,8 @@
 from __future__ import absolute_import, unicode_literals
-import os
+from django.conf import settings
 from celery import Celery
+import os
+
 
 # set the default Django settings module for the 'celery' program.
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'suziScraper.settings')
@@ -14,4 +16,4 @@ app = Celery('suziScraper')
 app.config_from_object('django.conf:settings', namespace='CELERY')
 
 # Load task modules from all registered Django app configs.
-app.autodiscover_tasks()
+app.autodiscover_tasks(lambda: settings.INSTALLED_APPS)
