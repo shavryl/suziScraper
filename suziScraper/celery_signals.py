ff70f64a86a037ebc8b27b7dc376a0aef48dfece
thereShouldBeaLogin <gavrilchenko.stanislav@gmail.com>
2018-11-08 15:23:47 +0200

    signals

--- suziScraper/celery_signals.py
+++ suziScraper/celery_signals.py
@@ -1,4 +1,5 @@
 from celery.signals import after_task_publish
+from celery.signals import celeryd_after_setup
 
 
 @after_task_publish.connect(sender='suziScraper.tasks.add')
@@ -7,3 +8,10 @@ def task_sent_handler(sender=None, headers=None, body=None, **kwargs):
     # using the task protocol version 2
     info = headers if 'task' in headers else body
     print('after_task_publish for task id {info[id]}'.format(info=info,))
+
+
+@celeryd_after_setup.connect
+def setup_direct_queue(sender, instance, **kwargs):
+    # sender is the nodename of the worker
+    queue_name = '{0}.dq'.format(sender)
+    instance.app.amqp.queues.select_add(queue_name)
